f616f8fcf96f611df32acce181de3b0af7b40bcf
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-05 13:16:34 +0530

    cors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { getSession, signIn, signOut, signUp } from "../controllers/userControllers.js";
 import { handleUser } from "../middleware/authMiddlewares.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const userRoutes = Router();
 
+userRoutes.use(cors(
+    {
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+        origin: [process.env.ADMIN_ORIGIN, process.env.MAIN_ORIGIN],
+        optionsSuccessStatus: 201
+    }
+))
 userRoutes.get("/auth", handleUser, getSession);
 userRoutes.post("/auth", signIn);
 userRoutes.post("/sign-up", signUp);
