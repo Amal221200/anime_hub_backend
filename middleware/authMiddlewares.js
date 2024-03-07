3bcf65b59c98b8d8777a51db75df735fcb4077f1
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-07 10:52:13 +0530

    minor change

--- middleware/authMiddlewares.js
+++ middleware/authMiddlewares.js
@@ -8,6 +8,7 @@ dotenv.config()
 export const handleUser = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
+
         if (!token) {
             return res.status(401).json("Unauthorized user");
         }
