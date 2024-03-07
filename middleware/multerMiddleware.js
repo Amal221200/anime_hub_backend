18921771898126bd8aebb4c894b5666bb04ba25f
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-07 11:26:25 +0530

    minor change

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,9 +1,8 @@
 import multer from "multer";
-import path from "path";
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, path.join("../public/uploads"))
+        cb(null, "../public/uploads")
     },
     filename(req, file, cb) {
         cb(null, `${Math.floor(Math.random() * 50)}_${file.originalname}`)
